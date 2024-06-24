3c08f4f355de3ee9cb46af92118cf088eb1234f3
Wai Yan <waiyankyawsdk1999@gmail.com>
2024-06-24 23:41:46 +0630

    feat: register form

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,9 +2,9 @@
 
 # from django.shortcuts import render
 # from django.http import HttpResponseRedirect, HttpResponse
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 # from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib.auth import logout
+from django.contrib.auth import logout
 # from django.contrib import messages
 # from datetime import datetime
 
@@ -39,13 +39,53 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    data = {"userName":""}
+    return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
-# @csrf_exempt
-# def registration(request):
-# ...
+@csrf_exempt
+def registration(request):
+    context = {}
+    data = json.loads(request.body)
+    username = data['userName']
+    password = data['password']
+    first_name = data['firstName']
+    last_name = data['lastName']
+    email = data['email']
+    username_exist = False
+    email_exist = False
+    try:
+        # Check if user already exists
+        User.objects.get(username=username)
+        username_exist = True
+    except:
+        # If not, simply log this is a new user
+        logger.debug("{} is new user".format(username))
+
+    try:
+        # Check if user already exists
+        User.objects.get(email=email)
+        email_exist = True
+    except:
+        # If not, simply log this is a new user
+        logger.debug("{} is new email".format(email))
+
+    # If it is a new user
+    if username_exist:
+        data = {"userName":username,"error":"Already Registered"}
+        return JsonResponse(data)
+    elif email_exist:
+        data = {"email":email,"error":"Already Registered"}
+        return JsonResponse(data)
+
+    # Create user in auth_user table
+    user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
+    # Login the user and redirect to list page
+    login(request, user)
+    data = {"userName":username,"status":"Authenticated"}
+    return JsonResponse(data)
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -65,7 +65,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static')
+            os.path.join(BASE_DIR,'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -140,6 +142,8 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static')
+    os.path.join(BASE_DIR,'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/build'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
